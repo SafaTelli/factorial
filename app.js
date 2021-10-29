2e2244b661d53ca7ee5e1b2c0bdd45abfc1f1e9a
Safa Telli <safa.telli@gmail.com>
2021-10-29 15:54:25 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+
+app.get('/factorial/:n', (req, res) => {
+    try {
+        const input = req.params.n;
+
+        if(input==0) {
+            throw new Error('Input must be greater than zero.');
+        }
+        res.json({
+            input: input,
+            output: factorial(input)
+        });
+    } catch(err) {
+        res.status(500).send('the parameter for factorial must be an integer greater than zero.');
+    }
+})
+
+function factorial(n) {
+    let f = 1 ;
+
+    for(var i = n; i >= 1; i--){
+        f = f * i;
+      }
+    return f;
+}
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
